26abaa06b41dedda7ba5b2f789353725d2dc3678
Michael Synan <hello@michaelsynan.com>
2024-12-30 17:27:54 -0500

    Feat: fixing scrolling, adding additional component instances

--- app/components/Home/Carousel.vue
+++ app/components/Home/Carousel.vue
@@ -1,52 +1,45 @@
-<script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-import { useWindowScroll } from '@vueuse/core'
+  <script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import { useEventListener } from '@vueuse/core'
 
-const el = ref<HTMLElement | null>(null);
+  const props = defineProps<{
+    items: Array;
+    direction: 'left' | 'right'
+  }>()
 
-const { y } = useWindowScroll();
+  const el = ref<HTMLElement | null>(null)
 
-const items = [
-  '/video-photography-website.webp',
-  '/dashboard.webp',
-  '/hero-image.webp',
-  '/facial-serum-website.webp',
-  '/daily-grind.webp',
-  '/mobile-app.webp',
-  '/jazz.webp',
-  '/coding-nurse.webp',
-  '/yoga-pilates.webp',
-  '/michael.webp',
-];
-
-onMounted(() => {
-  watch(y, (newValue) => {
-    if (el.value) {
-      el.value.scrollLeft = newValue * 0.7;
-    }
-  }, { immediate: true });
-});
+  onMounted(() => {
+    useEventListener(window, 'scroll', () => {
+      if (el.value) {
+        const maxScrollLeft = el.value.scrollWidth - el.value.clientWidth;
+        if (props.direction === 'left') {
+          el.value.scrollLeft = window.scrollY * 0.7;
+        } else {
+          el.value.scrollLeft = maxScrollLeft - (window.scrollY * 0.7);
+        }
+      }
+    })
+  })
 </script>
 
-<template>
-  <div ref="el" class="scroll-container flex flex-nowrap overflow-x-scroll gap-6">
-    <div v-for="item in items" :key="item" class="shrink-0">
-      <img :src="item" class="h-64 object-contain" style="width: auto;">
+  <template>
+    <div ref="el" class="scroll-container flex flex-nowrap overflow-x-scroll gap-6">
+      <div v-for="item in props.items" :key="item" class="shrink-0">
+        <img :src="item" class="h-64 object-contain" style="width: auto;">
+      </div>
     </div>
-  </div>
-</template>
+  </template>
 
 <style scoped>
 .scroll-container {
+  scroll-behavior: auto;
 
   &::-webkit-scrollbar {
     display: none;
   }
 
   scrollbar-width: none;
-
   -ms-overflow-style: none;
-
-  scroll-behavior: smooth;
 }
 </style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,7 +7,44 @@
       <HomeProblem class="flex-none w-full" />
       <HomeSolutions class="flex-none w-full" />
       <HomeProcess class="flex-none w-full" />
-      <HomeCarousel />
+      <div class="flex flex-col gap-10">
+        <HomeCarousel :direction="'right'" :items="[
+          '/video-photography-website.webp',
+          '/dashboard.webp',
+          '/hero-image.webp',
+          '/facial-serum-website.webp',
+          '/daily-grind.webp',
+          '/mobile-app.webp',
+          '/jazz.webp',
+          '/coding-nurse.webp',
+          '/yoga-pilates.webp',
+          '/michael.webp',
+        ]" />
+        <HomeCarousel :direction="'left'" :items="[
+          '/video-photography-website.webp',
+          '/dashboard.webp',
+          '/hero-image.webp',
+          '/facial-serum-website.webp',
+          '/daily-grind.webp',
+          '/mobile-app.webp',
+          '/jazz.webp',
+          '/coding-nurse.webp',
+          '/yoga-pilates.webp',
+          '/michael.webp',
+        ]" />
+        <HomeCarousel :direction="'right'" :items="[
+          '/video-photography-website.webp',
+          '/dashboard.webp',
+          '/hero-image.webp',
+          '/facial-serum-website.webp',
+          '/daily-grind.webp',
+          '/mobile-app.webp',
+          '/jazz.webp',
+          '/coding-nurse.webp',
+          '/yoga-pilates.webp',
+          '/michael.webp',
+        ]" />
+      </div>
       <HomeLeads class="flex-none w-full" />
       <HomeTestimonials class="flex-none w-full" />
       <HomeFAQ class="flex-none w-full" />
