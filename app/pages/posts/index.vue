1439a95ab48309325a34e1d95967ce5e1bf2ab5e
Michael Synan <hello@michaelsynan.com>
2025-05-09 16:34:27 -0400

    feat: deploying

--- app/pages/posts/index.vue
+++ app/pages/posts/index.vue
@@ -17,7 +17,7 @@ const { data: blogs } = await useAsyncData('documents-list', () => {
 
       <!-- Content positioned at the bottom of the hero section -->
       <div id="hero" class="relative z-10 mt-auto w-full px-6 md:px-16 pb-12 md:pb-16">
-        <h1 class="text-4xl md:text-6xl font-bold font-delight text-left">Design, Technology & Philosophy</h1>
+        <h1 class="text-4xl md:text-6xl font-bold font-delight text-left testing">Design, Technology & Philosophy</h1>
         <div class="text-lg md:text-2xl switzer opacity-50 text-left mt-3 md:mt-4"></div>
       </div>
     </div>
