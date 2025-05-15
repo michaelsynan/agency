034a59bd4a1bea99bbe0d8edc9dccaf6eb2f3cf1
Michael Synan <hello@michaelsynan.com>
2025-05-14 20:04:09 -0400

    style: fixing header on blog

--- app/pages/posts/index.vue
+++ app/pages/posts/index.vue
@@ -8,7 +8,7 @@ const { data: blogs } = await useAsyncData('documents-list', () => {
 
 <template>
   <main class="text-white min-h-screen">
-    <div class="w-full h-[66vh] relative flex flex-col bg-juju-950/50 overflow-hidden">
+    <div class="w-full h-[50vh] md:h-[66vh] relative flex flex-col bg-juju-950/50 overflow-hidden">
       <!-- Background image with blur effect -->
       <div class="absolute inset-0 bg-image opacity-30 blur-image !bg-fixed"></div>
 
@@ -17,7 +17,9 @@ const { data: blogs } = await useAsyncData('documents-list', () => {
 
       <!-- Content positioned at the bottom of the hero section -->
       <div id="hero" class="relative z-10 mt-auto w-full px-6 md:px-16 pb-12 md:pb-16">
-        <h1 class="text-4xl md:text-6xl font-bold font-delight text-left testing">Design, Technology & Philosophy</h1>
+        <h1 class="text-4xl max-w-[15ch] leading-normal md:text-6xl font-bold font-delight text-left testing">Design,
+          Technology &
+          Philosophy</h1>
         <div class="text-lg md:text-2xl switzer opacity-50 text-left mt-3 md:mt-4"></div>
       </div>
     </div>
