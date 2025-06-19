b31e7bdf8c47aabed28d01bf2e970140cb2e79c0
Michael Synan <hello@michaelsynan.com>
2025-06-19 18:23:11 -0400

    updated font and image format

--- app/components/Home/Carousel.vue
+++ app/components/Home/Carousel.vue
@@ -1,35 +1,35 @@
-  <script setup lang="ts">
-  import { onMounted, ref } from 'vue'
-  import { useEventListener } from '@vueuse/core'
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useEventListener } from '@vueuse/core'
 
-  const props = defineProps<{
-    items: Array;
-    direction: 'left' | 'right'
-  }>()
+const props = defineProps<{
+  items: Array;
+  direction: 'left' | 'right'
+}>()
 
-  const el = ref<HTMLElement | null>(null)
+const el = ref<HTMLElement | null>(null)
 
-  onMounted(() => {
-    useEventListener(window, 'scroll', () => {
-      if (el.value) {
-        const maxScrollLeft = el.value.scrollWidth - el.value.clientWidth;
-        if (props.direction === 'left') {
-          el.value.scrollLeft = window.scrollY * 0.7;
-        } else {
-          el.value.scrollLeft = maxScrollLeft - (window.scrollY * 0.7);
-        }
+onMounted(() => {
+  useEventListener(window, 'scroll', () => {
+    if (el.value) {
+      const maxScrollLeft = el.value.scrollWidth - el.value.clientWidth;
+      if (props.direction === 'left') {
+        el.value.scrollLeft = window.scrollY * 0.7;
+      } else {
+        el.value.scrollLeft = maxScrollLeft - (window.scrollY * 0.7);
       }
-    })
+    }
   })
+})
 </script>
 
-  <template>
-    <div ref="el" class="scroll-container flex flex-nowrap overflow-x-scroll gap-8">
-      <div v-for="item in props.items" :key="item" class="shrink-0">
-        <img :src="item" class="h-64 object-contain" style="width: auto;">
-      </div>
+<template>
+  <div ref="el" class="scroll-container flex flex-nowrap overflow-x-scroll gap-8">
+    <div v-for="item in props.items" :key="item.src" class="shrink-0">
+      <NuxtImg :src="item.src" :alt="item.alt" class="h-64 object-contain" style="width: auto;" placeholder />
     </div>
-  </template>
+  </div>
+</template>
 
 <style scoped>
 .scroll-container {
