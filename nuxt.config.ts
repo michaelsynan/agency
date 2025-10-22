89a5dcf9e818fd9ad0e2b1fc1920bd6c35f9a3fc
Michael Synan <hello@michaelsynan.com>
2025-10-21 21:52:11 -0400

    Feat: added schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineLocalBusiness } from "nuxt-schema-org/schema";
+
 export default defineNuxtConfig({
   modules: [
     "@nuxtjs/robots",
@@ -73,6 +75,92 @@ export default defineNuxtConfig({
     },
   },
   seo: {
+    schemaOrg: {
+      identity: [
+        defineLocalBusiness({
+          "@type": "ProfessionalService",
+          name: "Formwork Studios",
+          description:
+            "Formwork Studios creates human-centered digital solutions blending strategy, technology, and design to solve complex problems for you and your business.",
+          url: "https://formworkstudios.com",
+          address: {
+            streetAddress: "966 Ridge Avenue",
+            addressLocality: "Pennsylvania",
+            addressRegion: "PA",
+            postalCode: "18510",
+            addressCountry: "US",
+          },
+          telephone: "+1-800-657-6893",
+          email: "hello@formworkstudios.com",
+          image: [
+            "https://formworkstudios.com/pa.jpg",
+            "https://formworkstudios.com/china-town-nyc.webp",
+          ],
+          logo: "https://formworkstudios.com/bset-web-design-company-ever.png",
+          openingHoursSpecification: [
+            {
+              dayOfWeek: [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+              ],
+              opens: "08:00:00",
+              closes: "21:00:00",
+            },
+          ],
+          isAccessibleForDisabled: true,
+          sameAs: [
+            "https://www.facebook.com/formworkstudios",
+            "https://www.instagram.com/formworkstudios/",
+          ],
+        }),
+        defineLocalBusiness({
+          "@type": "ProfessionalService",
+          name: "Formwork Studios NYC",
+          description:
+            "NYC branch of Formwork Studios, providing digital solutions, web design, and marketing for New York City businesses.",
+          url: "https://formworkstudios.com",
+          address: {
+            streetAddress: "61-25 97th Street 12R",
+            addressLocality: "Queens",
+            addressRegion: "NY",
+            postalCode: "",
+            addressCountry: "US",
+          },
+          telephone: "+1-800-657-6893",
+          email: "hello@formworkstudios.com",
+          image: [
+            "https://formworkstudios.com/china-town-nyc.webp",
+            "https://formworkstudios.com/pa.jpg",
+          ],
+          logo: "https://formworkstudios.com/bset-web-design-company-ever.png",
+          openingHoursSpecification: [
+            {
+              dayOfWeek: [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+              ],
+              opens: "08:00:00",
+              closes: "21:00:00",
+            },
+          ],
+          isAccessibleForDisabled: true,
+          sameAs: [
+            "https://www.facebook.com/formworkstudios",
+            "https://www.instagram.com/formworkstudios/",
+          ],
+        }),
+      ],
+    },
     meta: {
       description:
         "Formwork Studios creates human-centered digital solutions blending strategy, technology, and design to solve complex problems for you and your business.",
